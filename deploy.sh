5675ca2f354418e4a95bd4c885cd6c9e0b26438b
WiZLite <mikuto.matsuo@icloud.com>
2023-03-14 20:45:56 +0900

    update workflow

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-docker build -t test/multi-client -f ./client/Dockerfile ./client
-docker build -t test/multi-server -f ./server/Dockerfile ./server
-docker build -t test/multi-worker -f ./worker/Dockerfile ./worker
-docker push test/multi-client
-docker push test/multi-server
-docker push test/multi-worker
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=test/multi-server
